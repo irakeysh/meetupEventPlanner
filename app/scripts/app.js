3704c604472f98596a847164ed7528d7df9303b2
irakeysh <tan.jubatus@gmail.com>
2016-05-15 18:15:56 +0530

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name eventPlannerApp
+ * @description
+ * # eventPlannerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('eventPlannerApp', [
+    'ngAnimate',
+    'ngAria'
+  ]);
