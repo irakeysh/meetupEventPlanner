b4afe65cde03adf9d00f9f9b21ee704dc4353700
irakeysh <tan.jubatus@gmail.com>
2016-05-23 11:22:26 +0530

    Routing working after authentication

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -16,14 +16,15 @@
  */
 angular.module('eventPlannerApp')
   .controller('LoginCtrl',LoginCtrl);
-  LoginCtrl.$inject = ['$scope', '$rootScope', '$location', 'APP_SETTINGS', '$firebaseAuth'];
+  LoginCtrl.$inject = ['$scope', '$rootScope', '$location', 'APP_SETTINGS', '$firebaseAuth','$state'];
 
-  function LoginCtrl($scope, $rootScope, $location, APP_SETTINGS, $firebaseAuth) {
+  function LoginCtrl($scope, $rootScope, $location, APP_SETTINGS, $firebaseAuth,$state) {
     var auth = firebase.auth();
     $scope.registerUser = function(){
     	auth.createUserWithEmailAndPassword($scope.newUserEmail,$scope.newUserPassword)
     			.then(function(userData){
     				console.log("User Registration successfull with the following details",userData.uid);
+             $state.go('home');
             
     			})
     			.catch(function(error){
@@ -34,7 +35,8 @@ angular.module('eventPlannerApp')
     $scope.loginUser = function(){
       auth.signInWithEmailAndPassword($scope.activeUserEmail,$scope.activeUserPassword)
           .then(function(userData){
-            console.log("User Login successfull with the following details",userData.uid)
+            console.log("User Login successfull with the following details",userData.uid);
+            $state.go('home');
           })
           .catch(function(error){
             console.log("Error with the error:",error);
